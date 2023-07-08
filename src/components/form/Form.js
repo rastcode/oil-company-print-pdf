ccb3c5bfd71bf2d137c893e1454d82e0ac54e852
ali <nematiali1377@gmail.com>
2023-07-08 17:14:55 -0300

    v3

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -7,7 +7,6 @@ import Info from "./Info";
 import Pelak from "./Pelak";
 import Bak from "./Bak";
 
-
 function Form() {
   const { information, date } = useContext(MainContext);
   const {
@@ -23,11 +22,8 @@ function Form() {
     total,
   } = information;
 
- 
-
   return (
-    
- <div id="table-container" className="form-container">
+    <div id="table-container" className="form-container">
       <div className="border-container">
         <div className="date-container">
           <div className="tarikh">{date && date.toString().split(" ", 1)}</div>
@@ -42,37 +38,38 @@ function Form() {
         </div>
         <div className="company-name">شرکت ملی پخش فراورده های نفتی</div>
         <div className="info-container">
-          <Info className={"info"} placeholder={"نام کاربری"} value={username} />
-          <Info className={"info"} placeholder={"نقطه مرزی:"} value={border ? border : "پرویزخان"} />
-          
-          
-          
+          <Info
+            className={"info"}
+            placeholder={"نام کاربری: "}
+            value={username}
+          />
+          <Info
+            className={"info"}
+            placeholder={"نقطه مرزی: "}
+            value={border ? border : "پرویزخان"}
+          />
+
           <div className="info">
             <p>شناسه: </p>
             <div className="identety">
               <p>{identety}</p>
-              <p>ترانشیپ ورودی</p>
+              <p className="tranship">ترانشیپ ورودی</p>
             </div>
           </div>
           <Pelak className={"info"} placeholder={"پلاک:"} />
         </div>
         <div className="gas-container">
           <div className="baks">
-            <Bak name={"باک 1 :"}value={bak1}/>
-            <Bak name={"باک 2 :"}value={bak2}/>
-            
-            
-            
+            <Bak name={"باک 1 :"} value={bak1} />
+            <Bak name={"باک 2 :"} value={bak2} />
           </div>
           <div className="baks">
-          <Bak name={"باک 3 :"}value={bak3}/>
-          <Bak name={"باک 4 :"}value={bak4}/>
-            
+            <Bak name={"باک 3 :"} value={bak3} />
+            <Bak name={"باک 4 :"} value={bak4} />
           </div>
           <div className="baks">
-          <Bak name={"عرضه :"}value={arze}/>
-          <Bak name={"حجم کل :"}value={total ? ` ${total}Lit ` : "0"}/>
-            
+            <Bak name={"عرضه :"} value={arze} />
+            <Bak name={"حجم کل :"} value={total ? ` ${total}Lit ` : "0"} />
           </div>
         </div>
         <div className="warning-container">اطلاعات ارسال شد</div>
@@ -81,8 +78,6 @@ function Form() {
         </div>
       </div>
     </div>
-    
-   
   );
 }
 
